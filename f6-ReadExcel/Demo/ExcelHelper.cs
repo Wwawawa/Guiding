63eceb6cfcad9fce7c3c9c07e92ec3fd61710f07
Wwawawa <421299099@qq.com>
2017-09-27 13:39:20 +0800

    Create ExcelHelper.cs

---
+++ f6-ReadExcel/Demo/ExcelHelper.cs
@@ -0,0 +1,58 @@
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+
+namespace ReadExcel
+{
+    public class ExcelHelper
+    {
+
+        public static XSSFWorkbook GetExcelFile(string filePath)
+        {
+            XSSFWorkbook wk = null;
+            try
+            {                
+                using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    wk = new XSSFWorkbook(fs);
+                    fs.Close();
+                }
+                return wk;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static ISheet GetExcelSheet(XSSFWorkbook wk, string sheetname)
+        {
+            try
+            {
+                ISheet sheet = wk.GetSheet(sheetname);
+                return sheet;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static string GetCellValue(ISheet sheet,int column, int row)
+        {
+            try
+            {
+                ICell cell = sheet.GetRow(row).GetCell(column);
+                return cell.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
